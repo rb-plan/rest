5019e141f66e3f186ab94e0bbfc96475de7f12d5
Shaojing <scandyboat@gmail.com>
2024-07-05 09:08:21 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+mkdir -p target
+
+# Set Go modules
+export GO111MODULE=on
+
+# Build for arm64-linux
+GOARCH=arm64 GOOS=linux go build -o target/rest-arm64-linux main.go
+
+# Build for arm64-freebsd
+GOARCH=arm64 GOOS=freebsd go build -o target/rest-arm64-freebsd main.go
+
+# Build for x86_64-linux
+GOARCH=amd64 GOOS=linux go build -o target/rest-x86_64-linux main.go
+
+# Build for x86_64-freebsd
+GOARCH=amd64 GOOS=freebsd go build -o target/rest-x86_64-freebsd main.go
+
+echo "Build complete."
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+)
+
+func getNetworkInterfaces() (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	var result string
+	for _, iface := range interfaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			result += fmt.Sprintf("%s: %s\n", iface.Name, addr.String())
+		}
+	}
+	return result, nil
+}
+
+func getCurrentTime() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+func getUsers() string {
+
+	hostname, _ := os.Hostname()
+	usersTxt := fmt.Sprintf("%s\n", hostname)
+	users, _ := host.Users()
+
+	for _, user := range users {
+		// data, _ := json.MarshalIndent(user, "", " ")
+		// usersTxt += string(data)
+
+		usersTxt += fmt.Sprintf("%s@%s %s %s\n", user.User, user.Host, user.Terminal, time.Unix(int64(user.Started), 0).Format("2006-01-02 15:04:05"))
+	}
+	return usersTxt
+}
+
+func getMemory() string {
+
+	v, _ := mem.VirtualMemory()
+
+	return fmt.Sprintf("%v Bytes (%f%% used)", v.Total, v.UsedPercent)
+}
+
+func getBootTime() string {
+
+	info, err := host.Info()
+	if err != nil {
+		fmt.Println("Failed to get host info:", err)
+		return ""
+	}
+
+	return time.Unix(int64(info.BootTime), 0).Format("2006-01-02 15:04:05")
+}
+
+func getSysInfo() string {
+
+	memTotal := getMemory()
+	cpuCores := runtime.NumCPU()
+
+	return fmt.Sprintf("boot: %s\nsyst: %s %s\ncpus: %d\nmemo: %s\n",
+		getBootTime(), runtime.GOOS, runtime.GOARCH, cpuCores, memTotal)
+}
+
+func main() {
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	networkInfo, err := getNetworkInterfaces()
+	if err != nil {
+		log.Fatalf("failed to get network interfaces: %v", err)
+	}
+
+	// Create a paragraph for the network interfaces
+	pNetwork := widgets.NewParagraph()
+	pNetwork.Title = "Network Interfaces"
+	pNetwork.BorderStyle.Fg = ui.ColorRed
+	pNetwork.TitleStyle.Fg = ui.ColorRed
+	pNetwork.TextStyle.Fg = ui.ColorRed
+	pNetwork.Text = networkInfo
+
+	// Create a paragraph for the clock
+	pClock := widgets.NewParagraph()
+	pClock.Title = "System Info"
+	pClock.BorderStyle.Fg = ui.ColorYellow
+	pClock.TitleStyle.Fg = ui.ColorYellow
+	pClock.TextStyle.Fg = ui.ColorYellow
+	pClock.Text = fmt.Sprintf("date: %s\n%s", getCurrentTime(), getSysInfo())
+
+	// Create a paragraph for the Users
+	pUsers := widgets.NewParagraph()
+	pUsers.Title = "Users"
+	pUsers.BorderStyle.Fg = ui.ColorGreen
+	pUsers.TitleStyle.Fg = ui.ColorGreen
+	pUsers.TextStyle.Fg = ui.ColorGreen
+	pUsers.Text = getUsers()
+
+	// Function to update the clock
+	uiEvents := ui.PollEvents()
+	ticker := time.NewTicker(time.Second).C
+	go func() {
+		for {
+			select {
+			case <-ticker:
+				pClock.Text = fmt.Sprintf("date: %s\n%s", getCurrentTime(), getSysInfo())
+				ui.Render(pClock)
+				pUsers.Text = getUsers()
+				ui.Render(pUsers)
+			case e := <-uiEvents:
+				if e.Type == ui.KeyboardEvent {
+					return
+				}
+			}
+		}
+	}()
+
+	// Initial rendering of the widgets
+	width, height := ui.TerminalDimensions()
+	pNetwork.SetRect(0, 0, width/2, height/2)
+	pClock.SetRect(width/2, 0, width, height/2)
+	pUsers.SetRect(0, height/2, width, height)
+
+	ui.Render(pNetwork, pClock, pUsers)
+
+	// Block main goroutine until a keyboard event is received
+	<-uiEvents
+}
